6ec6444529b4ad7063936c07bafcb6ccfdbb227a
Leboxgang <thevirusbruh>
2025-01-11 21:42:39 -0800

    test 2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { '/tmp/holberton':
-  path    => '/tmp/holberton',
+file { '/tmp/school':
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
