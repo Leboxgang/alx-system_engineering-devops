df56522b5ed982c53650a8dcd6ff0e8ef4a2c6aa
Leboxgang <thevirusbruh@gmail.com>
2025-02-27 21:21:04 +0000

    api adv

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests as r
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "custom-user-agent"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
     }
     response = r.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
